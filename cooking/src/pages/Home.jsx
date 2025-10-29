6c59a05c07adcc0e85f128a7c5e0ff831ab6be20
JunKamanda <jkamanda062@gmail.com>
2025-10-29 08:19:18 +0100

    COOKING-APP version 1 : Terminé

--- cooking/src/pages/Home.jsx
+++ cooking/src/pages/Home.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Title from '../components/Title';
-import Navigation from '../components/Navigation';
 import Card from '../components/Card';
 
 const Home = () => {

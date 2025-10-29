2ab2206e6a0d95c9eb641d22abbffdfae661120b
JunKamanda <jkamanda062@gmail.com>
2025-10-29 08:11:09 +0100

    ✨ F02: COOKING-APP > version 1 > terminé

--- cooking/src/components/Card.jsx
+++ cooking/src/components/Card.jsx
@@ -1,38 +1,49 @@
 import React, { useEffect, useState } from "react";
-// import Recette from "./Recette";
-import cookImg from "../assets/cook.jpg";
-
+import Recette from "./Recette";
 import axios from "axios";
 
 const Card = () => {
   //state
   const [meals, setMeals] = useState([]);
+  const [inputValue, setInputValue] = useState("");
 
   //comportement
   useEffect(() => {
     axios
-      .get("https://www.themealdb.com/api/json/v1/1/search.php?s=tomato")
+      .get(`https://www.themealdb.com/api/json/v1/1/search.php?s=${inputValue}`)
       .then((res) => {
-        setMeals(res.data.meals);
+        setMeals(res.data.meals || []);
         console.log(res.data.meals);
       })
       .catch((error) => {
         console.error("Erreur lors de la récupération :", error);
       });
-  }, []);
+  }, [inputValue]);
+
+  const handleChange = (e)=>{
+    setInputValue(e.target.value);
+  }
 
   //render
   return (
     <div className="card">
+      <div className="navigation">
+        <form>
+          <input
+            type="text"
+            id="nom"
+            placeholder="Tapez le nom d'un aliment (en anglais)"
+            value={inputValue}
+            onChange={handleChange}
+          />
+        </form>
+      </div>
       <ul>
-        {meals.map((meal) => (
-          <li key={meal.idMeal}>
-            <h3>{meal.strMeal}</h3>
-            <p>Origin : {meal.strArea}</p>
-            <img src={meal.strMealThumb} alt="image's cook" />
-            <p className="description">{meal.strInstructions}</p>
-          </li>
-        ))}
+        {meals && meals.length > 0 ? (
+          meals.map((meal) => <Recette key={meal.idMeal} meal={meal} />)
+        ) : (
+          <p>Chargement...</p>
+        )}
       </ul>
     </div>
   );
--- cooking/src/components/Recette.jsx
+++ cooking/src/components/Recette.jsx
@@ -1,18 +1,13 @@
 import React from "react";
-import cookImg from "../assets/cook.jpg";
 
-const Recette = () => {
+const Recette = ({meal}) => {
   return (
     <div>
       <li>
-        <h3>name</h3>
-        <p>Origin</p>
-        <img src={cookImg} alt="image's cook" />
-        <p className="description">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae
-          consectetur deleniti nam magnam. Illum quos labore reprehenderit iste
-          praesentium molestias,
-        </p>
+        <h3>{meal.strMeal}</h3>
+        <p>Origin : {meal.strArea}</p>
+        <img src={meal.strMealThumb} alt="image's cook" />
+        <p className="description">{meal.strInstructions}</p>
       </li>
     </div>
   );
